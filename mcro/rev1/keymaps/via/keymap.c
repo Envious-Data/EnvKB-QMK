c1fa3fd1784e6cd1e5f655cc81c2da9c5211d7ec
EnviousData <Enviousdata@gmail.com>
2021-12-27 16:54:23 +0000

    another via fix but still no worky.

--- mcro/rev1/keymaps/via/keymap.c
+++ mcro/rev1/keymaps/via/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "via.h"
 #include "pico_eeprom.h"
 
 
@@ -26,3 +27,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     },
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    return true;
+}
