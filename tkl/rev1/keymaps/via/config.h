cb29b146b1d59006d4b24ddc257976d0b5479ee5
EnviousData <Enviousdata@gmail.com>
2022-07-02 15:17:32 +0100

    started work on VIA(L)

---
+++ tkl/rev1/keymaps/via/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xXX, 0xXX, 0xXX, 0xXX, 0xXX, 0xXX, 0xXX, 0xXX}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
